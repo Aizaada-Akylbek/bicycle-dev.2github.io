d3018e498f798c5441fc14be095a816409eb0cb1
aizaada <akylbekova.aizaada@gmail.com>
2024-05-08 16:43:55 +0600

    added 2 pages

---
+++ src/components/ui/devOperations/DevOperations.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './DevOperations.css'
+
+const DevOperations = () => {
+  return (
+    <div className="development container">
+      <h4 className="poppins">
+        Do your development
+        <br /> operations need a level-up?
+      </h4>
+      <p>Let's dive in to see the value Bicycle Dev can provide you</p>
+      <NavLink to="/employees">
+        {" "}
+        <button className="main-btn">Contact our experts</button>
+      </NavLink>
+
+    </div>
+  );
+};
+
+export default DevOperations;
