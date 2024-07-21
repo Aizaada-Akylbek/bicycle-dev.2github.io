a8f473199a2466d119fcd57eeba9a01bf1b6c020
aizaada <akylbekova.aizaada@gmail.com>
2024-07-21 16:51:07 +0600

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/bicycle-dev.github.io/',
+  // base:'/bicycle-dev.github.io/',
   plugins: [react()],
-
+  build: {
+    outDir: 'dist', // папка для сборки
+    rollupOptions: {
+      // Дополнительные настройки Rollup, если требуется
+    },
+  }
 })
